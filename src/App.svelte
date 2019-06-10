4a7af5dc10cc60122faea76e018671e178006fc2
adam <adam.sobiesak@gmail.com>
2019-06-10 09:38:57 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Navbar from "./Navbar.svelte";
+  import Player from "./Player.svelte";
+
+  let players = [
+    { name: "Jan Janowski", points: 33 },
+    { name: "Harrison Ford", points: 55 },
+    { name: "Indiana Jones", points: 72 }
+  ];
+</script>
+
+<Navbar />
+<div class="container">
+  {#if players.length === 0}
+    <p>No players</p>
+  {:else}
+    {#each players as player}
+      <Player name={player.name} points={player.points} />
+    {/each}
+  {/if}
+</div>
---
+++ src/Player.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let name;
+  export let points;
+  let showControls = false;
+
+  const addPoints = () => (points += 1);
+  const removePoints = () => (points -= 1);
+  const toggleControls = () => (showControls = !showControls);
+</script>
+
+<style>
+  h1 {
+    color: #204f6e;
+  }
+</style>
+
+<div class="card">
+  <h1>
+     {name}
+    <button class="btn btn-sm" on:click={toggleControls}>
+      {#if showControls}-{:else}+{/if}
+    </button>
+  </h1>
+  <h3>points: {points}</h3>
+  {#if showControls}
+    <button class="btn" on:click={addPoints}>+1</button>
+    <button class="btn btn-dark" on:click={removePoints}>-1</button>
+    <input type="number" bind:value={points} />
+  {/if}
+</div>
